63642c4fdad76fa2ec34097082b3aa1a182f4699
fchignolli <102098077+BrianWBurke@users.noreply.github.com>
2023-01-06 13:53:31 -0300

    Capturando URL

--- 2 - Preço Placas de Vídeo/Capturando preços de Placas e Salvando.py
+++ 2 - Preço Placas de Vídeo/Capturando preços de Placas e Salvando.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import math
+import re
 
 url = 'https://www.kabum.com.br/hardware/placa-de-video-vga'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
@@ -16,7 +17,7 @@ qtd = qtd_itens[:index]
 
 ultima_pagina = math.ceil(int(qtd) / 36)
 
-dic_produtos = {'site':[], 'marca':[], 'preco':[]}
+dic_produtos = {'site':[], 'marca':[], 'preco':[], 'link':[]}
 
 for i in range(1, ultima_pagina+1):
     url_pag = f'https://www.kabum.com.br/hardware/placa-de-video-vga?page_number={i}&page_size=20&facet_filters=&sort=most_searched'
@@ -26,21 +27,21 @@ for i in range(1, ultima_pagina+1):
     
     for produto in produtos:
         site = 'Kabum'
-        marca = produto.find('span', class_=re.compile('nameCard')).get_text().strip()
+        marca = produto.find('span', class_=re.compile('nameCard')).get_text().strip()    
         preco = produto.find('span', class_=re.compile('priceCard')).get_text().strip()
-        
-        print(marca, preco)
+        if preco == 'R$ ---':
+            preco = 'Indisponível'
+        link = url_pag        
+        print(i, site, marca, preco, link)
         
         dic_produtos['site'].append(site)
         dic_produtos['marca'].append(marca)
         dic_produtos['preco'].append(preco)
+        dic_produtos['link'].append(link)
 
 
 df = pd.DataFrame(dic_produtos)
 df.to_csv('C:\\Users\\Luis Felipe\\Documents\\CSV Placa de Vídeos\\preco_cadeira.csv', encoding='utf-8', sep=';')
 
-xls = input('C:\\Users\\Luis Felipe\\Documents\\CSV Placa de Vídeos\\preco_cadeira.csv')
-read_file = pd.read_csv(xls)
-teclado = xls.replace("csv","xlsx")
-read_file.to_excel(xls, index = None, header=True)
-print("Arquivo convertido!")
+read_file = pd.read_csv (r'C:\\Users\\Luis Felipe\\Documents\\CSV Placa de Vídeos\\preco_cadeira.csv', encoding='utf-8', sep=';')
+read_file.to_excel (r'C:\\Users\\Luis Felipe\\Documents\\CSV Placa de Vídeos\\preco_cadeira.xls', index = None, header=True)
