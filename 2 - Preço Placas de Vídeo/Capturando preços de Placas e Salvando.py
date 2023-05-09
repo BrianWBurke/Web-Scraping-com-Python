2f50c71daf934cfe1399767379aa9223a2a4944f
Brian <102098077+BrianWBurke@users.noreply.github.com>
2023-05-09 10:23:59 -0300

    Update Capturando preços de Placas e Salvando.py

--- 2 - Preço Placas de Vídeo/Capturando preços de Placas e Salvando.py
+++ 2 - Preço Placas de Vídeo/Capturando preços de Placas e Salvando.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import math
-import re
 
 url = 'https://www.kabum.com.br/hardware/placa-de-video-vga'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
